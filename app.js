f6fd3f358f853932562a2925757c46afb9e87f2f
Anth Richardson <anth@technicallycompatible.com>
2016-06-23 10:23:27 +0100

    Added clipchamp.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("test");
+clipchamp.preload();
+
+var clipchampEle = document.getElementById("clipchamp-container");
+if(clipchampEle) {
+	clipchamp(clipchampEle, {
+		label: "Record video",
+		title: "Record your answer",
+		logo: "assets/tc_logo.svg",
+		color: "#000",
+		resolution: "480p",
+		compression: "medium",
+		inputs: ["camera"],
+		output: "dummy",
+		camera: {
+			limit: 300,
+		}
+	});
+}
